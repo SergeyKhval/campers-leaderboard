25559451221b06bb7472cb812275fb004191474a
Sergey <sergeykhval@gmail.com>
2017-06-14 23:07:25 +0300

    chore: initial commit

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom'
+import 'normalize.css';
+import './style.css';
+
+class Main extends Component {
+
+}
+
+render(<Main />, document.getElementById('root'));
